10da41ef9888bcddb72c5520bfa0f9002f22dc4f
Amadou Barry <barryamadou191@gmail.com>
2024-01-03 10:25:48 +0000

    first commit

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// import { setupCounter } from './counter.ts'
+var myVar: number = 4
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `<div>${myVar}</div>`
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
